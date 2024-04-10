9d604a105491da2351827c038cbeaa662ec4d226
Sergey Ivanov <aureolu@gmail.com>
2024-04-10 07:42:09 +0300

    add images & rectangles

--- main.py
+++ main.py
@@ -20,27 +20,34 @@ cracker_speed = 10
 
 cracker_img = pygame.image.load('images/cracker_s.png')
 cracker_rect = cracker_img.get_rect()
+cracker_rect.x = WIDTH // 2
+cracker_rect.y = HEIGHT - cracker_size * 2
 
 enemy_size = 50
-enemy_pos = [random.randint(0, WIDTH - enemy_size), 0]
 enemy_speed = 5
-enemies = [enemy_pos]
-
 enemy_img = pygame.image.load('images/glass_of_milk_s.png')
 enemy_rect = enemy_img.get_rect()
+enemy_rect.x = random.randint(0, WIDTH - enemy_size)
+enemy_rect.y = 0
+enemies = [enemy_rect.x, enemy_rect.y]
+
+bg_image = pygame.image.load('images/cracker_bg_01_s.jpg')
 
 # Функция отрисовки объектов
 def draw_objects():
-    WIN.fill(WHITE)
-    pygame.draw.rect(WIN, BLACK, (cracker_pos[0], cracker_pos[1], cracker_size, cracker_size))
+    WIN.blit(bg_image, (0, 0))
+
+    WIN.blit(cracker_img, cracker_rect)
 
     for enemy in enemies:
-        pygame.draw.rect(WIN, BLACK, (enemy[0], enemy[1], enemy_size, enemy_size))
+        WIN.blit(enemy_img, enemy_rect)
 
 # Основной игровой цикл
 clock = pygame.time.Clock()
 running = True
 while running:
+    # Отображение фоновой картинки
+
     clock.tick(30)
 
     for event in pygame.event.get():
@@ -48,18 +55,22 @@ while running:
             running = False
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and cracker_pos[0] - cracker_speed > 0:
-        cracker_pos[0] -= cracker_speed
-    if keys[pygame.K_RIGHT] and cracker_pos[0] + cracker_size + cracker_speed < WIDTH:
-        cracker_pos[0] += cracker_speed
+    if keys[pygame.K_LEFT] and cracker_rect.x - cracker_speed > 0:
+        cracker_rect.x -= cracker_speed
+    if keys[pygame.K_RIGHT] and cracker_rect.x + cracker_size + cracker_speed < WIDTH:
+        cracker_rect.x += cracker_speed
+    if keys[pygame.K_UP] and cracker_rect.y - cracker_speed > 0:
+        cracker_rect.y -= cracker_speed
+    if keys[pygame.K_DOWN] and cracker_rect.y + cracker_size + cracker_speed < WIDTH:
+        cracker_rect.y += cracker_speed
 
     for enemy in enemies:
-        if enemy[1] < HEIGHT:
-            enemy[1] += enemy_speed
+        if enemy_rect.y < HEIGHT:
+            enemy_rect.y += enemy_speed
         else:
-            enemy[0] = random.randint(0, WIDTH - enemy_size)
-            enemy[1] = 0
-        if enemy[1] + enemy_size > cracker_pos[1] and cracker_pos[0] < enemy[0] + enemy_size and cracker_pos[0] + cracker_size > enemy[0]:
+            enemy_rect.x = random.randint(0, WIDTH - enemy_size)
+            enemy_rect.y = 0
+        if enemy_rect.y + enemy_size > cracker_rect.y and cracker_rect.x < enemy_rect.x + enemy_size and cracker_rect.x + cracker_size > enemy_rect.x:
             enemies.remove(enemy)
 
     draw_objects()
